948e5bb7f9d0e297b4abe7156b41eb34d8d06a2e
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-08-31 14:41:58 +0300

    add files

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import App from "./App.js";
 
 describe("App tests", () => {
   it("should contains the heading 1", () => {
