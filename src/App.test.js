5645986d3571a29225b9402c6f96507c96f2baaa
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-09-11 12:59:12 +0300

    Latest change
    Fix node and npm version

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,15 @@
-import { render, screen } from "@testing-library/react";
+import { renderer } from "react-test-renderer";
+
 import App from "./App.js";
 import sum from "./sum";
 import { picture } from "./products";
 
 describe("App tests", () => {
   it("adds 1 + 2 to equal 3", () => {
-    render(<App />);
+    renderer.create(<App />);
     const linkElement = screen.getByText(/learn react/i);
     expect(linkElement).toBeInTheDocument();
     expect(sum(1, 2)).toBe(3);
-    expect(picture(2000, 1)).toBe(2024);
+    expect(picture(2022, 1)).toBe(2024);
   });
 });
